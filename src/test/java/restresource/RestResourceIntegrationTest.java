43c08979194087ead2835ac07d3372c8ec0f373f
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-01-15 22:44:15 -0200

    Some code refactory.

--- src/test/java/restresource/RestResourceIntegrationTest.java
+++ src/test/java/restresource/RestResourceIntegrationTest.java
@@ -1,24 +1,17 @@
 package restresource;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
+import static restresource.support.TestUtils.assertRaises;
+import static restresource.support.TestUtils.assertRaisesStatusException;
 
 import java.util.List;
 
 import org.junit.Test;
 
-import restresource.exceptions.ClientException;
-import restresource.exceptions.ForbiddenAccessException;
-import restresource.exceptions.ResourceInvalidException;
 import restresource.exceptions.ResourceNotFoundException;
-import restresource.exceptions.ServerException;
-import restresource.exceptions.StatusException;
-import restresource.exceptions.UnauthorizedAccessException;
 import restresource.support.Person;
 import restresource.support.StatusCode;
 import restresource.utils.Proc;
-import static restresource.support.TestUtils.assertRaises;
 
 public class RestResourceIntegrationTest {
 	@Test
@@ -27,71 +20,12 @@ public class RestResourceIntegrationTest {
 		assertEquals("John", p.getName());
 	}
 
-	private void testRaiseStatusException(Proc block) {
-		for (int i = 400; i < 600; i++) {
-			StatusException ce = null;
-			try {
-				block.call(i);
-			} catch(StatusException e) {
-				ce = e;
-			}
-			if (ce == null)
-				fail("Expected 'find' with response code " + i + " to raise " +
-						StatusException.class);
-			assertEquals(i, ce.getStatus());
-			if (i < 500) {
-				assertTrue("Exception raised should be ClientException",
-						ce instanceof ClientException);
-				switch (i) {
-				case 401:
-					assertTrue("Expected exception to be instance of UnauthorizedAccessException (401)",
-							ce instanceof UnauthorizedAccessException);
-					break;
-				case 403:
-					assertTrue("Expected exception to be instance of ForbiddenAccessException (403)",
-							ce instanceof ForbiddenAccessException);
-					break;
-				case 404:
-					assertTrue("Expected exception to be instance of ResourceNotFoundException (404)",
-							ce instanceof ResourceNotFoundException);
-					break;
-				case 422:
-					assertTrue("Expected exception to be instance of ResourceInvalidException (422)",
-							ce instanceof ResourceInvalidException);
-					break;
-				}
-			} else
-				assertTrue("Exception raised should be ServerException",
-						ce instanceof ServerException);
-		}
-	}
-
 	@Test
 	public void testFindRaisesCorrectException() {
-		testRaiseStatusException(new Proc() {
+		assertRaisesStatusException(new Proc() {
 			@Override
 			public void call(Object... args) {
 				RestResource.find(args[0], StatusCode.class);
-			}
-		});
-	}
-
-	@Test
-	public void testAllRaisesCorrectException() {
-		testRaiseStatusException(new Proc() {
-			@Override
-			public void call(Object... args) {
-				RestResource.all(StatusCode.class, "status=" + args[0]);
-			}
-		});
-	}
-
-	@Test
-	public void testCreateRaisesCorrectException() {
-		testRaiseStatusException(new Proc() {
-			@Override
-			public void call(Object... args) {
-				RestResource.save(new StatusCode((Integer) args[0]));
 			}
 		});
 	}
@@ -112,12 +46,32 @@ public class RestResourceIntegrationTest {
 	}
 
 	@Test
+	public void testAllRaisesCorrectException() {
+		assertRaisesStatusException(new Proc() {
+			@Override
+			public void call(Object... args) {
+				RestResource.all(StatusCode.class, "status=" + args[0]);
+			}
+		});
+	}
+
+	@Test
 	public void testCreate() {
 		Person p = new Person();
 		p.setName("Test");
 		p = RestResource.save(p);
 		assertEquals(1, p.getId());
 		assertEquals("Test", p.getName());
+	}
+
+	@Test
+	public void testCreateRaisesCorrectException() {
+		assertRaisesStatusException(new Proc() {
+			@Override
+			public void call(Object... args) {
+				RestResource.save(new StatusCode((Integer) args[0]));
+			}
+		});
 	}
 
 	@Test
--- src/test/java/restresource/support/TestUtils.java
+++ src/test/java/restresource/support/TestUtils.java
@@ -2,6 +2,15 @@ package restresource.support;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+import restresource.exceptions.ClientException;
+import restresource.exceptions.ForbiddenAccessException;
+import restresource.exceptions.ResourceInvalidException;
+import restresource.exceptions.ResourceNotFoundException;
+import restresource.exceptions.ServerException;
+import restresource.exceptions.StatusException;
+import restresource.exceptions.UnauthorizedAccessException;
 import restresource.utils.Proc;
 
 public class TestUtils {
@@ -16,5 +25,44 @@ public class TestUtils {
 		assertNotNull("Expected to catch an exception of " + klass
 				+ ", but no exception was received.", exc);
 		assertEquals(klass, exc.getClass());
+	}
+
+	public static void assertRaisesStatusException(Proc block) {
+		for (int i = 400; i < 600; i++) {
+			StatusException ce = null;
+			try {
+				block.call(i);
+			} catch(StatusException e) {
+				ce = e;
+			}
+			if (ce == null)
+				fail("Expected 'find' with response code " + i + " to raise " +
+						StatusException.class);
+			assertEquals(i, ce.getStatus());
+			if (i < 500) {
+				assertTrue("Exception raised should be ClientException",
+						ce instanceof ClientException);
+				switch (i) {
+				case 401:
+					assertTrue("Expected exception to be instance of UnauthorizedAccessException (401)",
+							ce instanceof UnauthorizedAccessException);
+					break;
+				case 403:
+					assertTrue("Expected exception to be instance of ForbiddenAccessException (403)",
+							ce instanceof ForbiddenAccessException);
+					break;
+				case 404:
+					assertTrue("Expected exception to be instance of ResourceNotFoundException (404)",
+							ce instanceof ResourceNotFoundException);
+					break;
+				case 422:
+					assertTrue("Expected exception to be instance of ResourceInvalidException (422)",
+							ce instanceof ResourceInvalidException);
+					break;
+				}
+			} else
+				assertTrue("Exception raised should be ServerException",
+						ce instanceof ServerException);
+		}
 	}
 }
